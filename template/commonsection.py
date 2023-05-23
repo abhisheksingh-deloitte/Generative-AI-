0f8b5d742fc0584865b4512a34a6a4752a18c4da
Abhishek Singh <ABHISHEKSINGH29@DELOITTE.COM>
2023-05-23 18:53:26 +0530

    Extract the data from excel and send the data to openAi ,get the response,generate the template.

---
+++ template/commonsection.py
@@ -0,0 +1,61 @@
+import openai
+import openpyxl
+from docx import Document
+
+
+def fetch_data_and_write_to_doc(sheet_name, headings, column_names, output_doc):
+    # Load the Excel file
+    workbook = openpyxl.load_workbook('../Utils/excel.xlsx')
+
+    # Select the specified sheet
+    sheet = workbook[sheet_name]
+
+    # Find the column indices based on the column names
+    column_indices = {}
+    for heading, column_name in zip(headings, column_names):
+        column_index = None
+        for cell in sheet[1]:
+            if cell.value == column_name:
+                column_index = cell.column
+                break
+        if column_index is None:
+            print(f"Column '{column_name}' not found.")
+            return
+        column_indices[heading] = column_index
+
+    # Create a new document
+    doc = Document()
+
+    # Write the headings to the document
+    for heading in headings:
+        doc.add_paragraph(heading)
+
+    # Fetch the data from the specified columns and write it to the document in front of the respective headings
+    num_rows = sheet.max_row
+    for row_index in range(2, num_rows + 1):
+        for heading, column_index in column_indices.items():
+            column_value = sheet.cell(row=row_index, column=column_index).value
+            doc.add_heading(f"{heading}", level=1)
+            # Fetch data from OpenAI
+            openai.api_key = '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'
+            prompt = "User Requirement - The functional specification document will specify the details for generating a customer invoice form for Canada. The form type identified is Adobe Forms in BTP, triggered by the business document. This document will outline the necessary information and requirements to create a custom invoice form tailored to the specific needs of Canadian customers."
+            spec = "Choose the content which is applicable for user requirement from below and create the assumptions of a functional specification document in not more than 60 words:"
+            response = openai.Completion.create(
+                engine='text-davinci-003',
+                prompt=prompt + spec + f"{column_value}",
+                max_tokens=2000
+            )
+            data = response.choices[0].text.strip()
+            doc.add_paragraph(data)
+
+            print(data)
+
+    # Save the document
+    doc.save(output_doc)
+    print(f"Data written to '{output_doc}' successfully.")
+
+
+# Example usage
+headings = ["Introduction", "Assumptions"]
+column_names = ["fs-general_fields-requirements", "fs-general_fields-assumptions"]
+fetch_data_and_write_to_doc("Sheet1", headings, column_names, "new_output.docx")
