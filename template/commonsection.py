8c8aea123d6777cbbec235a44f8a95772406ac89
Abhishek Singh <ABHISHEKSINGH29@DELOITTE.COM>
2023-05-24 14:22:24 +0530

    concatenation of two column value

--- template/commonsection.py
+++ template/commonsection.py
@@ -32,17 +32,20 @@ def fetch_data_and_write_to_doc(sheet_name, headings, column_names, output_doc,c
 
     # Fetch the data from the specified columns and write it to the document in front of the respective headings
     num_rows = sheet.max_row
+    doc.add_heading(f"{heading}", level=1)
     for row_index in range(2, num_rows + 1):
+        values = {}  # Dictionary to store the column values
         for heading, column_index in column_indices.items():
             column_value = sheet.cell(row=row_index, column=column_index).value
-            doc.add_heading(f"{heading}", level=1)
+            values[heading] = column_value
             # Fetch data from OpenAI
-            openai.api_key = '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'
+            openai.api_key = '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'
+            prompt_values = " ".join([str(value) for value in values.values()])
             prompt = "User Requirement - The functional specification document will specify the details for generating a customer invoice form for Canada. The form type identified is Adobe Forms in BTP, triggered by the business document. This document will outline the necessary information and requirements to create a custom invoice form tailored to the specific needs of Canadian customers."
             spec = f"Choose the content which is applicable for user requirement from below and create the {contents} of a functional specification document in not more than 60 words:"
             response = openai.Completion.create(
                 engine='text-davinci-003',
-                prompt=prompt + spec + f"{column_value}",
+                prompt=prompt + spec + f" {prompt_values}",
                 max_tokens=2000
             )
             data = response.choices[0].text.strip()
@@ -56,4 +59,4 @@ def fetch_data_and_write_to_doc(sheet_name, headings, column_names, output_doc,c
 # Example usage
 headings = ["Assumptions"]
 column_names = ["fs-general_fields-assumptions"]
-fetch_data_and_write_to_doc("Sheet1", headings, column_names, "new_output.docx","assumptions")
+fetch_data_and_write_to_doc("Sheet1", headings, column_names, "output.docx","assumptions")
